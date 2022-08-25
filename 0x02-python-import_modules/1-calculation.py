04ad6c434c1df285bafa228003753251992b0d25
Loycemary <loycemary2012@gmail.com>
2022-08-25 15:01:37 -0700

    Tasks

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from calculator_1 import calculator
+
+if _name_ == "_main_":
+    a = 10
+    b = 5
+
+    print ('{:d} + {:d} = {:d}.format(a,b, add(a,b)))
+    print ('{:d} - {:d} = {:d}.format(a,b, subtract(a,b)))
+    print ('{:d} * {:d} = {:d}.format(a,b, multiply(a,b)))
+    print ('{:d} / {:d} = {:d}.format(a,b, divide(a,b)))
+
